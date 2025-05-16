47877149597d6073e0a50a32ec8e6e5f9a3c3106
IamEduardok <jeduardowork@gmail.com>
2025-05-16 16:10:29 -0300

    Parte de Contatos finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, MatSlideToggleModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'] // <- corrigido aqui
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'portifolio';
